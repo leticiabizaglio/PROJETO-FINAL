c316e6761e6ba16e05b7e59791ceaa501c0d8dc3
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-21 10:53:48 -0300

    feat: create casas routes

---
+++ src/routers/casas.routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+    getCasas,
+    getCasaId,
+    createCasa,
+    updateCasa,
+    deleteCasa
+} from "../controllers/pets.controller.js";
+
+const casaRoutes = Router();
+
+casaRoutes.get("/", getCasas);
+
+casaRoutes.get("/:id", getCasaId);
+
+casaRoutes.post("/", createCasa);
+
+casaRoutes.put("/:id", updateCasa);
+
+casaRoutes.delete("/:id", deleteCasa);
+
+export default casaRoutes;

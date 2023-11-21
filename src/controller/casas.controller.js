a3b811ed8c47bd06f177bb8f106ca7021a5ab6fb
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-21 10:48:52 -0300

    feat: create controller of casas

---
+++ src/controller/casas.controller.js
@@ -0,0 +1,74 @@
+import { Casa } from "../models/casas/Casa.js";
+import { casasList } from "../models/casas/CasasList.js";
+
+const instanciaLista = new casasList();
+
+export const getCasas = (req, res) =>{
+    const casas = instanciaLista.getAllCasas();
+    if(!casas.length){
+        res.status(404).json({message:"Não existem casas cadastradas.", cadastros: `${casas.length}` })
+    } else{
+        res.status(200).json({message:"Operação realizada com sucesso.", status:"OK", data:casas, cadastros: `${casas.length}` })
+    }
+}
+export const getCasaId = (req, res) =>{
+    const { id } = req.params;
+
+    const casa = instanciaLista.getCasaById(id);
+
+    if(!casa) {
+        return res.status(404).send({
+             message: "Not Found.",
+             
+            });
+    }
+    return res.status(200).send({ message: `id: ${id}!`, data:casa });
+}
+export const createCasa = (req, res) => {
+    const { nome, imagem, origem} = req.body;
+
+    if(!nome || !imagem || !origem){
+        return res.status(404).send({message: "Not Found"});
+    }
+    if(isURLValid(imagem) === false) {
+        return res.status(400).send({ message: "a imagem deve ser uma URL valida" });
+    }
+    const casaInstancia = new Casa(nome, imagem, origem)
+
+    instanciaLista.createCasa(casaInstancia);
+
+    return res.status(200).send({ message: "Registrado com sucesso", data: casaInstancia});
+
+};
+export const updateCasa = (req, res) => {
+    const { id } = req.params;
+    const { nome, imagem, origem } = req.body;
+    if(!nome || !origem || !imagem){
+        return res.status(404).send({message: "Not Found"});
+    }
+    const casa = instanciaLista.updateCasa(id, nome, imagem, origem);
+    if(!casa){
+        return res.status(404).send({ message: "Not Found"});
+    }
+
+    return res.status(200).send({message: `id: ${id}`, data: pet});
+};
+export const deleteCasa = (req, res) => {
+    const { id } = req.params;
+
+    const casa = instanciaLista.getPetById(id);
+
+    if(!casa) {
+        return res.status(404).send({ message: "Not Found."});
+    }
+
+    instanciaLista.excludeCasa(id);
+    return res.status(200).send({ message: `id: ${id}!`, data: casa });
+}
+export const isURLValid = (url) => {
+    if(url.match(/\.(jpeg|jpg|gif|png)$/) != null){
+        return true;
+    } else {
+        return false;
+    }
+}

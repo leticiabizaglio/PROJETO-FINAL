80a31a00db4cabd1ce17fe936b7fa607b2f9fb15
JuliaFee <julia.silva133@aluno.senai.br>
2023-11-21 10:56:43 -0300

    feat: link casaRoutes in Index.js

--- src/routers/index.routes.js
+++ src/routers/index.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import rotaPersonagens from "./personagens.routes.js";
+import casaRoutes from "./casas.routes.js";
 
 const rotas = Router();
 
 rotas.use("/personagens", rotaPersonagens);
+rotas.use("/casas", casaRoutes);
 
 rotas.get("/", (req, res) => {
     return res.status(200).send({ message: "O servidor está funcionando!" });

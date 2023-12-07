0d24ba2261a01349a6dce8e0f58024c909eac9a5
Alineevang <aline.evangelista@aluno.senai.br>
2023-12-07 15:19:25 -0300

    feat: modificações

--- src/controller/personagens.controller.js
+++ src/controller/personagens.controller.js
@@ -108,7 +108,6 @@ export const atualizarPersonagem = (req, res) => {
     return res.status(200).send({message: `Personagem com id ${id} atualizado com sucesso!`});
 }
 
-
 // Função de deletar um personagem;
 export const deletarPersonagem = (req, res) => {
     const { id } = req.params;
